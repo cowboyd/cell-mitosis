a05d941fa0260d2d736bd197ee3197f6238b3431
Charles Lowell <cowboyd@thefrontside.net>
2009-05-07 08:36:13 -0400

    add mitosis require to init.rb

--- init.rb
+++ init.rb
@@ -1,5 +1,7 @@
 $:.unshift "#{File.dirname(__FILE__)}/lib"
 
+require 'mitosis'
+
 FileUtils.mkdir_p "#{Rails.public_path}/javascripts/gen"
 FileUtils.mkdir_p "#{Rails.public_path}/stylesheets/gen"
 
